90c4e3e64696d541a5037ca720244d7c296427cb
alvin joseph valdez <vlainlawliet@gmail.com>
2015-11-06 06:45:47 +0800

    added a editText for time and made it working to show a timepicker and change the format of datepicker dialog to be shown in the editText when the date is selected already.

--- app/src/main/java/com/serverus/paroah/activities/AddReminderActivity.java
+++ app/src/main/java/com/serverus/paroah/activities/AddReminderActivity.java
@@ -2,6 +2,7 @@ package com.serverus.paroah.activities;
 
 import android.app.DatePickerDialog;
 import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.widget.DatePicker;
 import android.widget.EdgeEffect;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.serverus.paroah.DB.MyDBHandler;
 import com.serverus.paroah.R;
@@ -25,6 +27,7 @@ import com.serverus.paroah.models.ListInfo;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddReminderActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -38,7 +41,13 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
     private Button saveBtn;
     private EditText reminderTitle;
     private EditText reminderDesc;
+    private EditText setTimeEdit;
     private RemindersAdapter adapter;
+    private Calendar c;
+
+
+    private int mHour;
+    private int mMinute;
 
     MyDBHandler dbHandler;
 
@@ -50,25 +59,16 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
 
         dbHandler = new MyDBHandler(this);
 
-        reminderTitle = (EditText) findViewById(R.id.edit_text_title);
-        reminderDesc = (EditText) findViewById(R.id.edit_text_desc);
-
-        setDateEdit = (EditText) findViewById(R.id.date_edit);
-        saveBtn = (Button) findViewById(R.id.save_btn);
+        reminderTitle   = (EditText) findViewById(R.id.edit_text_title);
+        reminderDesc    = (EditText) findViewById(R.id.edit_text_desc);
+        setDateEdit     = (EditText) findViewById(R.id.date_edit);
+        setTimeEdit     = (EditText) findViewById(R.id.time_edit);
+        saveBtn         = (Button) findViewById(R.id.save_btn);
 
         saveBtn.setOnClickListener(this);
+        setTimeEdit.setOnClickListener(this);
 
         setDate();
-
-        //database exist checker
-//        File database=this.getDatabasePath("paroah.db");
-//
-//        if (!database.exists()) {
-//            // Database does not exist so copy it from assets here
-//            Log.i("aoi", "Not Found");
-//        } else {
-//            Log.i("aoi", "Found");
-//        }
     }
 
     @Override
@@ -97,7 +97,7 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
         setDateEdit.setOnClickListener(this);
         setDateEdit.setText("");
 
-        final Calendar c = Calendar.getInstance();
+        c = Calendar.getInstance();
         datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -108,10 +108,49 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
                 selectedMonth = monthOfYear;
                 selectedDay = dayOfMonth;
 
-                setDateEdit.setText(selectedYear+" "+selectedDay+" "+selectedMonth);
+                setDateEdit.setText(formatDate(selectedYear, selectedMonth, selectedDay));
             }
         }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
 
+    }
+
+    private static String formatDate(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(0);
+        cal.set(year, month, day);
+        Date date = cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy");
+
+        return sdf.format(date);
+    }
+
+    private void setTime(){
+        TimePickerDialog.OnTimeSetListener timePicker=new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hourOfDay,
+                                  int minute) {
+                mHour =  hourOfDay;
+                mMinute =  minute;
+
+                String AM_PM ;
+                if(hourOfDay < 12) {
+                    AM_PM = "AM";
+
+                } else {
+                    AM_PM = "PM";
+                    mHour=mHour-12;
+                }
+
+                setTimeEdit.setText(mHour+":"+mMinute+" "+AM_PM);
+            }
+        };
+
+        // the 5th parameter is to tell if the timepicker is 24hr or not, this time its not
+        TimePickerDialog timePickerDialog =  new TimePickerDialog(this, timePicker,
+                c.get(Calendar.HOUR_OF_DAY),
+                c.get(Calendar.MINUTE),
+                false);
+
+        timePickerDialog.show();
     }
 
     @Override
@@ -122,6 +161,9 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
                 break;
             case R.id.save_btn:
                 saveReminder();
+                break;
+            case R.id.time_edit:
+                setTime();
                 break;
         }
     }
